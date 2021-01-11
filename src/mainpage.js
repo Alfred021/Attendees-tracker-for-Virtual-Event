73e5247e02bbbd4ec1e0cf70464da88a080dbc79
Alfred021 <alfredomedina10@hotmail.com>
2021-01-11 04:24:26 -0300

    Footer icons are now clickable
    Clickable icons sending the user to the respective pages.

--- src/mainpage.js
+++ src/mainpage.js
@@ -340,9 +340,9 @@ const MainPage = ({attendee, handleData, submit}) => {
         <footer>
             <p>© 2021</p>
             <div className="logos">
-                <img alt="github" src="https://cdns.iconmonstr.com/wp-content/assets/preview/2012/240/iconmonstr-github-1.png" width="50px" height="50px"></img>
-                <img alt="linkedin" src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-linkedin-circle-512.png" width="50px" height="50px"></img>
-                <img alt="email" src="https://icons-for-free.com/iconfiles/png/512/mail+message+new+send+icon-1320166532032442575.png" width="50px" height="50px"></img>
+                <a href="https://github.com/Alfred021/attendees-tracker-for-virtual-event" target="_blank" rel="noreferrer"><img alt="github" src="https://cdns.iconmonstr.com/wp-content/assets/preview/2012/240/iconmonstr-github-1.png" width="50px" height="50px"></img></a>
+                <a href="https://www.linkedin.com/in/alfredo-david-medina-lugo-177b461b4/" target="_blank" rel="noreferrer"><img alt="linkedin" src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-linkedin-circle-512.png" width="50px" height="50px"></img></a>
+                <a href="mailto: alfred.med.barca.9@gmail.com"><img alt="email" src="https://icons-for-free.com/iconfiles/png/512/mail+message+new+send+icon-1320166532032442575.png" width="50px" height="50px"></img></a>
             </div>
         </footer>
         </>
